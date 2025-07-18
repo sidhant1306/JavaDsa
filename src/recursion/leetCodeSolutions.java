94291cfb5796b9e4a39299b5d1d477cc95b3cf83
sidhant1306 <2023bcasidhant15127@poornima.edu.in>
2025-07-18 21:38:37 +0530

    Solved a new leetcode question on combinations

--- src/recursion/leetCodeSolutions.java
+++ src/recursion/leetCodeSolutions.java
@@ -3,14 +3,20 @@ import java.util.List;
 
 public class leetCodeSolutions {
     public static void main(String[] args) {
-        List<String> ans = new ArrayList<>();
-        String up = "";
-        if(up.isEmpty()){
-            System.out.println(ans);
-        }else{
-            phone("", up, ans);
-            System.out.println(ans);
-        }
+//        List<String> ans = new ArrayList<>();
+//        String up = "";
+//        if(up.isEmpty()){
+//            System.out.println(ans);
+//        }else{
+//            phone("", up, ans);
+//            System.out.println(ans);
+//        }
+
+//        int target = 4;
+//        int n = 2;
+//        int k = 6;
+//        int ans = dice("", target, n, k);
+//        System.out.println(ans);
 
     }
 
@@ -49,5 +55,27 @@ public class leetCodeSolutions {
 
             phone(p + ch, up.substring(1), result);
         }
+    }
+
+
+    //QUE 1155 (LEETCODE) : NUMBER OF DICE ROLLS WITH TARGET SUM:
+
+    // THE FUNCTION BELOW IS NOT FULLY OPTIMISED FOR LEETCODE INPUTS, AND IT STILL NEEDS THE USE OF DYNAMIC PROGRAMMING,
+    // BUT I CREATED THE FUNCTION TO AT LEAST WORK FOR SMALLER INPUTS FOR THE QUESTION USING ONLY RECURSION
+
+    public static  int dice(String p, int target, int n, int k){
+        if(target == 0 && n == 0){
+            return 1;
+        }
+
+        if(n == 0 && target != 0) return 0;
+
+        int count = 0;
+
+        for (int i = 1; i <= k && i <= target; i++) {
+            count = count + (dice(p + i, target - i, n - 1, k));
+        }
+
+        return count;
     }
 }
